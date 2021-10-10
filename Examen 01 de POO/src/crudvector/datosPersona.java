cef46286afc75dcc54515a0a3cd0e99e120f39c3
FrankChoquehuanca <mjb306diego@gmail.com>
2021-10-09 23:08:43 -0500

    ddd

---
+++ Examen 01 de POO/src/crudvector/datosPersona.java
@@ -0,0 +1,12 @@
+package src.crudvector;
+
+public class datosPersona {
+    
+    public String nombres;
+    public String codigo;
+    public String ap_paterno;
+    public String ap_materno;
+    //sssssssssssssssss
+    
+    
+}
